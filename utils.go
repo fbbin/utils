57a2cb8fc3f572dfd907e35eeeb0653d6ff3e1e2
方彬彬 <fbbin@foxmail.com>
2017-07-15 17:13:45 +0800

    添加

--- utils.go
+++ utils.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"crypto/md5"
 	"encoding/base64"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -11,6 +13,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func Ip2Long(ip string) (uint32, error) {
@@ -129,4 +132,157 @@ func Base64Encode(str string) string {
 func Base64Decode(str string) (string, error) {
 	s, e := base64.StdEncoding.DecodeString(str)
 	return string(s), e
+}
+
+// 截取字符串
+func subStr(str string, start int, length int) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+	if start > end {
+		start, end = end, start
+	}
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+	return string(rs[start:end])
+}
+
+func hashmd5(signString string) string {
+	hash := md5.New()
+	hash.Write([]byte(signString))
+	cipherStr := hash.Sum(nil)
+	return hex.EncodeToString(cipherStr)
+}
+
+// Format 跟 PHP 中 date 类似的使用方式，如果 ts 没传递，则使用当前时间
+func Format(format string, ts ...time.Time) string {
+	patterns := []string{
+		// 年
+		"Y", "2006", // 4 位数字完整表示的年份
+		"y", "06", // 2 位数字表示的年份
+
+		// 月
+		"m", "01", // 数字表示的月份，有前导零
+		"n", "1", // 数字表示的月份，没有前导零
+		"M", "Jan", // 三个字母缩写表示的月份
+		"F", "January", // 月份，完整的文本格式，例如 January 或者 March
+
+		// 日
+		"d", "02", // 月份中的第几天，有前导零的 2 位数字
+		"j", "2", // 月份中的第几天，没有前导零
+
+		"D", "Mon", // 星期几，文本表示，3 个字母
+		"l", "Monday", // 星期几，完整的文本格式;L的小写字母
+
+		// 时间
+		"g", "3", // 小时，12 小时格式，没有前导零
+		"G", "15", // 小时，24 小时格式，没有前导零
+		"h", "03", // 小时，12 小时格式，有前导零
+		"H", "15", // 小时，24 小时格式，有前导零
+
+		"a", "pm", // 小写的上午和下午值
+		"A", "PM", // 小写的上午和下午值
+
+		"i", "04", // 有前导零的分钟数
+		"s", "05", // 秒数，有前导零
+	}
+	replacer := strings.NewReplacer(patterns...)
+	format = replacer.Replace(format)
+
+	t := time.Now()
+	if len(ts) > 0 {
+		t = ts[0]
+	}
+	return t.Format(format)
+}
+
+func StrToLocalTime(value string) time.Time {
+	if value == "" {
+		return time.Time{}
+	}
+	zoneName, offset := time.Now().Zone()
+
+	zoneValue := offset / 3600 * 100
+	if zoneValue > 0 {
+		value += fmt.Sprintf(" +%04d", zoneValue)
+	} else {
+		value += fmt.Sprintf(" -%04d", zoneValue)
+	}
+
+	if zoneName != "" {
+		value += " " + zoneName
+	}
+	return StrToTime(value)
+}
+
+func StrToTime(value string) time.Time {
+	if value == "" {
+		return time.Time{}
+	}
+	layouts := []string{
+		"2006-01-02 15:04:05 -0700 MST",
+		"2006-01-02 15:04:05 -0700",
+		"2006-01-02 15:04:05",
+		"2006/01/02 15:04:05 -0700 MST",
+		"2006/01/02 15:04:05 -0700",
+		"2006/01/02 15:04:05",
+		"2006-01-02 -0700 MST",
+		"2006-01-02 -0700",
+		"2006-01-02",
+		"2006/01/02 -0700 MST",
+		"2006/01/02 -0700",
+		"2006/01/02",
+		"2006-01-02 15:04:05 -0700 -0700",
+		"2006/01/02 15:04:05 -0700 -0700",
+		"2006-01-02 -0700 -0700",
+		"2006/01/02 -0700 -0700",
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.Kitchen,
+		time.Stamp,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+	}
+
+	var t time.Time
+	var err error
+	for _, layout := range layouts {
+		t, err = time.Parse(layout, value)
+		if err == nil {
+			return t
+		}
+	}
+	panic(err)
+}
+
+func Shuffle(vals []interface{}) []interface{} {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ret := make([]interface{}, len(vals))
+	for i, randIndex := range r.Perm(len(vals)) {
+		ret[i] = vals[randIndex]
+	}
+	return ret
 }
